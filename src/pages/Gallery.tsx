0ff81a8019c72ce192fa6223cb78915b0774e54e
Mayo <mayocream39@yahoo.co.jp>
2021-10-15 10:44:46 +0800

    fix: frontend api url

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react'
 function Gallery(props: RouteComponentProps) {
   useEffect(() => {
     // 405
-    fetch(import.meta.env.VITE_API_URL + '/gallery').then((res) => console.log(res.json))
+    fetch(import.meta.env.VITE_API_URL + '/api/v0/gallery').then((res) => console.log(res.json))
   }, [])
   return <div>Gallery</div>
 }
