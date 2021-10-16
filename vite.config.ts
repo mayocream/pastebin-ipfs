be7a93b0ea7359a4a63ebc44b1478d4d8b92a8e9
Mayo <mayocream39@yahoo.co.jp>
2021-10-17 01:39:12 +0800

    chore(vite): build legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import macrosPlugin from 'vite-plugin-babel-macros'
+import legacy from '@vitejs/plugin-legacy'
 // @ts-ignore
 import { resolve } from 'path'
 
@@ -10,11 +11,17 @@ export default defineConfig({
     jsxFactory: 'jsx',
     jsxInject: 'import { jsx } from "@emotion/react"',
   },
-  plugins: [reactRefresh({ exclude: [/node_modules/] }), macrosPlugin()],
+  plugins: [
+    reactRefresh({ exclude: [/node_modules/] }),
+    macrosPlugin(),
+    legacy({
+      targets: ['chrome >= 70', 'firefox >= 72', 'edge >= 79', 'safari >= 13'],
+    }),
+  ],
   resolve: {
     alias: {
       // @ts-ignore
-      '@': resolve(__dirname, './src')
-    }
-  }
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
