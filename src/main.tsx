e47e4da7e0b8060300ee86d4aed6c39be7e0919d
Mayo <mayocream39@yahoo.co.jp>
2021-10-17 01:58:01 +0800

    chore(web): sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import './index.css'
 import App from './App'
+
+Sentry.init({
+  dsn: 'https://becf2bbe48f04c208b499233128f6776@o1041301.ingest.sentry.io/6010206',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <React.StrictMode>
